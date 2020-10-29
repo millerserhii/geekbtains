04a9010bbfe288487aed05034025b6e36dedf16e
millerserhii <48553674+millerserhii@users.noreply.github.com>
2020-10-29 16:40:52 +0200

    Delete 3-2.py

--- 3/3-2.py
+++
@@ -1,7 +0,0 @@
-
-def user_details(**kwargs):
-    return kwargs
-
-
-print(user_details(name = "Serhii", surname = "Miller", dob = 1992, city = "Moscow",
-                   email = "123@mail.com", phone = "89231212121"))
