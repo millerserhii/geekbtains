905d45c450e44a489af828dfe3794ff6fca1393c
millerserhii <48553674+millerserhii@users.noreply.github.com>
2020-10-29 16:42:51 +0200

    Delete 5-1.py

--- 5/5-1.py
+++
@@ -1,8 +0,0 @@
-
-with open ("task1.txt", "w", encoding="utf-8") as f:
-    while True:
-        usr_text = (input("Enter some text: "))
-        if usr_text:
-            print(usr_text, file=f)
-        else:
-            break
